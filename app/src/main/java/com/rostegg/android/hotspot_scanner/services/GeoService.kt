6f8eb558e3f66e960e70eb0038d95405cd404dc9
rostegg <rostik.nikolaenko@gmail.com>
2019-10-09 19:34:25 +0300

    💙

--- app/src/main/java/com/rostegg/android/hotspot_scanner/services/GeoService.kt
+++ app/src/main/java/com/rostegg/android/hotspot_scanner/services/GeoService.kt
@@ -1,7 +1,28 @@
 package com.rostegg.android.hotspot_scanner.services
 
 import android.content.Context
+import org.osmdroid.util.GeoPoint
+import kotlin.math.*
 
 class GeoService (context: Context) {
 
+    // get distance between two points in km
+    fun distance(from : GeoPoint, to: GeoPoint) : Double {
+
+        fun toRadians(value: Double) : Double {
+            return value * (Math.PI/180)
+        }
+
+        val R = 6371e3
+        val dLat = toRadians(to.latitude - from.latitude)
+        val dLon = toRadians(to.longitude - from.longitude)
+        val a =
+            sin(dLat/2) * sin(dLat/2) +
+            cos(toRadians(from.latitude)) * cos(toRadians(to.latitude)) *
+            sin(dLon/2) * sin(dLon/2)
+
+        var c = 2 * atan2(sqrt(a), sqrt(1-a))
+        var d = R * c
+        return  round(d) / 1000
+    }
 }
